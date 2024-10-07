9bec6cf5111f6e97eba5411737727e458dc76fa7
rayedison <rayedisonsalvatorsihombing@gmail.com>
2024-10-08 03:32:11 +0700

    Add Code File : index.php to main webapge

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List by Ray Edison</title>
+</head>
+<body>
+    <h1>To Do List</h1>
+
+    <form action="index.php">
+        <label for="toDo">To Do</label>
+        <input type="text" id="toDo">
+    </form>
+</body>
+</html>
